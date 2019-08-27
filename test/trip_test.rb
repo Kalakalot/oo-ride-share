75c32e0ff00967f39fb0c0adf68fc46ea8ee88c9
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-08-27 15:41:10 -0700

    fixed error, refactored start and end time validation, wrote test for it, passes. so far only one error with accurately loads driver information into drivers array

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -7,9 +7,12 @@ describe "Trip class" do
       end_time = start_time + 25 * 60 # 25 minutes
       @trip_data = {
         id: 8,
-        passenger: RideShare::Passenger.new(id: 1,
-                                            name: "Ada",
-                                            phone_number: "412-432-7640"),
+        passenger: RideShare::Passenger.new(
+          id: 1,
+          name: "Ada",
+          phone_number: "412-432-7640"
+        ),
+        
         start_time: start_time,
         end_time: end_time,
         cost: 23.45,
@@ -17,21 +20,46 @@ describe "Trip class" do
       }
       @trip = RideShare::Trip.new(@trip_data)
     end
-
+    
+    it "raises an error for end times that are earlier than start times" do
+      # arrange and act
+      start_time = Time.parse("2019-01-09 08:48:50 -0800")
+      end_time = Time.parse("2019-01-09 08:30:31 -0800")
+      # assert
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(
+          id: 1,
+          name: "Ada",
+          phone_number: "412-432-7640"
+        ),
+        
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+      expect do
+        if end_time < start_time
+        end.must_raise ArgumentError #RideShare::Trip.new()
+        # @trip = RideShare::Trip.new(@trip_data)
+      end
+    end
+    
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
     end
-
+    
     it "stores an instance of passenger" do
       expect(@trip.passenger).must_be_kind_of RideShare::Passenger
     end
-
+    
     it "stores an instance of driver" do
       skip # Unskip after wave 2
       expect(@trip.driver).must_be_kind_of RideShare::Driver
     end
-
-    it "raises an error for an invalid rating" do
+    
+    it "raises an error For an invalid rating" do
       [-3, 0, 6].each do |rating|
         @trip_data[:rating] = rating
         expect do
@@ -39,5 +67,6 @@ describe "Trip class" do
         end.must_raise ArgumentError
       end
     end
+    
   end
 end
